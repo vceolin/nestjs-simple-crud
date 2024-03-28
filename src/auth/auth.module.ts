f5573a3350e5378347cabb44a408fc89272fb1f3
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2024-03-27 21:45:12 -0300

    feat(auth): implement jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common/decorators/modules'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { UsersModule } from '@/users/users.module'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JwtModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,23 @@
 import { UnauthorizedException } from '@nestjs/common'
 import { UsersService } from '../users/users.service'
 import { Injectable } from '@nestjs/common/decorators/core'
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
 
   async signIn(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findOneByEmail(email)
     if (user?.password !== pass) {
       throw new UnauthorizedException()
     }
-    const { password, ...result } = user
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result
+    const payload = { sub: user.id, username: user.email }
+    return {
+      access_token: await this.jwtService.signAsync(payload)
+    }
   }
 }
