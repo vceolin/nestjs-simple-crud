e8465c3e9b3f3bbd222b9fcef75fb0be0fe2deb7
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2024-03-27 22:12:15 -0300

    docs(swagger): configure swagger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common/decorators/core'
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! Swagger is located at /api'
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
 
   const config = new DocumentBuilder()
-    .setTitle('Nestjs Simple Crud')
-    .setDescription(
-      'Nessa tarefa, vamos montar uma API simplificada no padrão RESTful com o Node.js e o Express, tendo um array simples como estrutura de dados.'
-    )
+    .setTitle('Social Network API')
+    .setDescription('Simple API for simulating a social network.')
     .setVersion('1.0')
-    .addTag('produtos')
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
