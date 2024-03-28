89c44e5582d38418753533bf0eaa0a4020ace4af
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2024-03-27 23:12:43 -0300

    feat(posts): add min and max length

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,4 +1,10 @@
+import { IsString, Length } from 'class-validator'
+
 export class CreatePostDto {
+  @IsString()
+  @Length(3, 20)
   title: string
+  @IsString()
+  @Length(3, 500)
   text: string
 }
