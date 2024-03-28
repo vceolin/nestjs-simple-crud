209d5d6b67c6470f8cce386353439f4ec622c656
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2024-03-27 21:13:15 -0300

    feat: create auth and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common/decorators/modules'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ProdutosModule } from './produtos/produtos.module'
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProdutosModule],
+  imports: [ProdutosModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService]
 })
