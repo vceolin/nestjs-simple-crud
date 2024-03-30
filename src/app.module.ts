2870e7c052a4f080bc665e86df60efab61905939
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2024-03-29 23:02:30 -0300

    fix: change imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common/decorators/modules'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MiddlewareConsumer, NestModule } from '@nestjs/common'
-import { AppLoggerMiddleware } from './logger/logger.middleware'
 import { ConfigModule } from '@nestjs/config'
 import { publicationsModule } from './publications/publications.module'
 import { AuthModule } from './auth/auth.module'
 import { CommentsModule } from './comments/comments.module'
 import { UsersModule } from './users/users.module'
+import { AppLoggerMiddleware } from './middlewares/logger.middleware'
 
 @Module({
   imports: [publicationsModule, AuthModule, UsersModule, CommentsModule, ConfigModule.forRoot()],
