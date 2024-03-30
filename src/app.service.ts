cdb1776df924d70eef2ab9a8ae8049cb13ad3f3f
Vitor Ceolin <39313422+vceolin@users.noreply.github.com>
2024-03-29 22:03:35 -0300

    refactor: remove auth from token

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common/decorators/core'
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Swagger is located at /api'
+    return `Hello World! Swagger is located at ${process.env.HOST}/api`
   }
 }
